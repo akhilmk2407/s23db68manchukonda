2d1a5908bf1028d023af910452f1332ce6ae5eb5
mkakhilkumar <mkakhilkumar@gmail.com>
2023-04-14 04:11:14 -0500

    Part2

--- controllers/handbags.js
+++ controllers/handbags.js
@@ -50,19 +50,40 @@ exports.handbags_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: handbags delete DELETE ' + req.params.id);
 };
 // Handle handbags update form on PUT.
-exports.handbags_update_put = function(req, res) {
+/*exports.handbags_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: handbags update PUT' + req.params.id);
-};
+};*/
 
 // VIEWS
 // Handle a show all view
     exports.handbags_view_all_Page = async function (req, res) {
         try {
         thehandbags = await handbags.find();
-        res.render('handbags', { title: 'handbags Search Results', results: thehandbags });
+        res.render('handbags', { title: 'handbags handbags Results', results: thehandbags });
         }
         catch (err) {
         res.status(500);
         res.send(`{"error": ${err}}`);
         }
     }
+
+    // Handle handbags update form on PUT.
+    exports.handbags_update_put = async function (req, res) {
+        console.log(`update on id ${req.params.id} with body 
+    ${JSON.stringify(req.body)}`)
+        try {
+            let toUpdate = await handbags.findById(req.params.id)
+            // Do updates of properties 
+            if (req.body.brand) toUpdate.brand = req.body.brand;
+            if (req.body.brand) toUpdate.model = req.body.model;
+            if (req.body.color) toUpdate.color = req.body.color;
+            if (req.body.price) toUpdate.price = req.body.price;
+            let result = await toUpdate.save();
+            console.log("Sucess " + result)
+            res.send(result)
+        } catch (err) {
+            res.status(500)
+            res.send(`{"error": ${err}: Update for id ${req.params.id} 
+    failed`);
+        }
+    };

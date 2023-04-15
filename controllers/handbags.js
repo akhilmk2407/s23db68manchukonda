f29d1e28a763c8e06ad950a39622f80175e53cc8
mkakhilkumar <mkakhilkumar@gmail.com>
2023-04-15 10:49:25 -0500

    Part5

--- controllers/handbags.js
+++ controllers/handbags.js
@@ -46,9 +46,18 @@ exports.handbags_create_post = async function(req, res) {
     }
 };
 // Handle handbags delete form on DELETE.
-exports.handbags_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: handbags delete DELETE ' + req.params.id);
-};
+// Handle handbags delete on DELETE.
+exports.handbags_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await handbags.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
 // Handle handbags update form on PUT.
 /*exports.handbags_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: handbags update PUT' + req.params.id);
@@ -87,3 +96,31 @@ exports.handbags_delete = function(req, res) {
     failed`);
         }
     };
+
+// Handle a show one view with id specified by query
+exports.handbags_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await handbags.findById( req.query.id)
+    res.render('handbagsdetail',
+   { title: 'handbags Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+// Handle building the view for creating a handbags.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.handbags_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('handbagscreate', { title: 'handbags Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
--- routes/handbags.js
+++ routes/handbags.js
@@ -5,4 +5,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', handbags_controlers.handbags_view_all_Page );
 
+/* GET detail handbags page */
+router.get('/detail', handbags_controlers.handbags_view_one_Page);
+
+/* GET create handbags page */
+router.get('/create', handbags_controlers.handbags_create_Page);
+
 module.exports = router;

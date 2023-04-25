386c6b6d7682853524063609164b367da4837ae9
mkakhilkumar <mkakhilkumar@gmail.com>
2023-04-25 02:16:49 -0500

    Updates are changed

--- models/handbags.js
+++ models/handbags.js
@@ -2,8 +2,15 @@ const mongoose = require("mongoose")
 const handbagsSchema = mongoose.Schema({
 brand: String,
 model: String,
-color: String,
-price: Number
+color: {
+    type: String,
+    required: true,
+    match: /^[a-zA-Z]+$/
+  },
+price:{
+    type:Number,
+    min: 1000,
+    max: 8000 } 
 
 })
 module.exports = mongoose.model("handbags",handbagsSchema)
--- routes/handbags.js
+++ routes/handbags.js
@@ -2,6 +2,18 @@ var express = require('express');
 const handbags_controlers= require('../controllers/handbags');
 var router = express.Router();
 
+
+// A little function to check if we have an authorized user and continue on 
+// or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
 /* GET home page. */
 router.get('/', handbags_controlers.handbags_view_all_Page );
 
@@ -9,12 +21,18 @@ router.get('/', handbags_controlers.handbags_view_all_Page );
 router.get('/detail', handbags_controlers.handbags_view_one_Page);
 
 /* GET create handbags page */
-router.get('/create', handbags_controlers.handbags_create_Page);
+router.get('/create',secured, handbags_controlers.handbags_create_Page);
 
 /* GET create update page */
-router.get('/update', handbags_controlers.handbags_update_Page);
+router.get('/update',secured,handbags_controlers.handbags_update_Page);
 
 /* GET delete handbags page */
-router.get('/delete', handbags_controlers.handbags_delete_Page);
+router.get('/delete',secured, handbags_controlers.handbags_delete_Page);
+
+/* GET update handbags page */
+router.get('/update',secured, handbags_controlers.handbags_update_Page);
+
+
+  
 
 module.exports = router;
